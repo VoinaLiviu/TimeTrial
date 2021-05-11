74471536e45b7c324e58d6bc795370b5676a0c1a
VoinaLiviu <liviuvoina79@gmail.com>
2021-05-12 01:05:18 +0300

    Committed entire project

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if(Input.GetKey("g"))
+        {
+            rb.AddForce(0, 6000 * Time.deltaTime, 0);
+        }
+
+    }
+}
